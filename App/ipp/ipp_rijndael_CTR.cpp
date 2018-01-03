6ec1de54a5830acf9649597889f5f95a7ab77a17
bob-young <583532940@qq.com>
2018-01-03 21:58:10 +0800

    add ctr in enclave

---
+++ App/ipp/ipp_rijndael_CTR.cpp
@@ -0,0 +1,42 @@
+#include "ipp_rijndael_CTR.h"
+
+
+IppStatus ipp_rijndael_CTR::encrypt(unsigned char* src,unsigned char* dest,int length,int* dest_len)
+{
+	IppStatus istate;
+	if(length==0){
+		printf("ipp rigndael encrypt input data error %s\n",ippcpGetStatusString(istate));
+		return -1;
+	}
+	unsigned char pCtrValue=0;
+	//int real_len=((length-1)/cfbBlkSize+1)*16;
+	int real_len=length;
+	*dest_len=real_len;
+	unsigned char* src_ec=(unsigned char*)calloc(real_len,sizeof(unsigned char));
+	memcpy(src_ec,src,length);
+	//istate=ippsAESEncryptECB(src_ec,dest,real_len,rijndael_context);
+	istate=ippsAESEncryptCTR(src_ec,dest,real_len,rijndael_context,&pCtrValue,1);
+	//ippsAESEncryptCTR(const Ipp8u* pSrc, Ipp8u* pDst, int srcLen,const IppsAESSpec* pCtx, Ipp8u* pCtrValue , int ctrNumBitSize );
+	//printf("encrypt:%p\n",rijndael_context);
+	if(istate != 0){
+		printf("ipp rigndael encrypt error %s\n",ippcpGetStatusString(istate));
+		return istate;	
+	}
+	//free(src_ec);
+	return istate;
+}
+
+IppStatus ipp_rijndael_CTR::decrypt(unsigned char* src,unsigned char* dest,int length)
+{
+	IppStatus istate;
+	//istate=ippsAESDecryptECB(src,dest,length,rijndael_context );
+	unsigned char pCtrValue=0;
+	istate=ippsAESDecryptCTR(src,dest,length,rijndael_context,&pCtrValue,1);
+	//IppStatus ippsAESDecryptCTR(const Ipp8u* pSrc, Ipp8u* pDst, int srcLen,const IppsAESSpec* pCtx, Ipp8u* pCtrValue, int ctrNumBitSize );
+	//printf("decrypt:%p\n",rijndael_context);	
+	if(istate != 0){
+		printf("ipp rigndael decrypt error %s\n",ippcpGetStatusString(istate));
+		return istate;	
+	}
+	return istate;
+}
---
+++ Enclave/ipp/ipp_rijndael_CTR.h
@@ -0,0 +1,19 @@
+#ifndef IPP_RIJNDAEL_CTR_H_
+#define IPP_RIJNDAEL_CTR_H_
+
+
+#include "ipp_rijndael.h"
+//#include "App.h"
+
+using namespace std;
+
+class ipp_rijndael_CTR:public ipp_rijndael
+{
+
+public:	
+	//
+	IppStatus encrypt(unsigned char* src,unsigned char* dest,int length,int* dest_len);
+	IppStatus decrypt(unsigned char* src,unsigned char* dest,int length);
+	//IppStatus init(const unsigned char* pwd,int pwdlen,enum Mode md=CBC);
+};
+#endif
